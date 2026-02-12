9af7ea5223666ca5d268a0f148859bc48c278d1c
Jordana Loreto <jordanaloreto45@gmail.com>
2026-02-12 10:07:13 -0300

    ++

---
+++ lineArray.php
@@ -0,0 +1,52 @@
+<?php
+function lerNumero($mensagem){
+    while (true){
+        echo $mensagem;
+        $valor = trim(fgets(STDIN));
+
+        if ($valor === '') {
+            return null;
+        }
+
+        if (filter_var($valor, FILTER_VALIDATE_FLOAT) !== false){
+            return (float) $valor;
+        }
+
+        echo "Digite apenas numeros.\n";
+    }
+}
+function lerNome($mensagem){
+    while (true){
+        echo $mensagem;
+        $valor = trim(fgets(STDIN));
+
+        if ($valor === '') {
+            return null;
+        }
+
+        return $valor; 
+    }
+}
+
+$l = lerNumero("L: ");
+$t = lerNome("T: ");
+
+$soma = 0;
+$media = 0;
+
+for($i = 0; $i <= 11; $i++){
+    for($j =0; $j <= 11; $j++){
+        $n = lerNumero('N: ');
+        if($i == $l){
+            $soma += $n;
+        }
+    }
+}
+if($t == 'S'){
+    echo "Soma = $soma\n";
+}else{
+    $media = $soma / 12;
+    echo "$media\n";
+}
+
+?>
